f546c5ef5944b6da8173fe22f8ae4b21afe57c11
harriet-fisher <106377699+harriet-fisher@users.noreply.github.com>
2023-10-19 05:18:32 +0000

    done square and square_root

--- app.rb
+++ app.rb
@@ -4,3 +4,23 @@ require "sinatra/reloader"
 get("/") do
   erb(:home)
 end
+
+get("/square/new") do
+  erb(:home)
+end
+
+get("/square_root/new") do
+  erb(:home)
+end
+
+post("/square_root/results") do
+  @number = params.fetch("number").to_i
+  @square_root = @number**0.5
+  erb(:flexible)
+end
+
+post("/square/results") do
+  @number = params.fetch("number").to_i
+  @square = @number**2
+  erb(:flexible)
+end
