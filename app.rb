f7583f289ab6b3c688449a3eda3a8573d61db465
harriet-fisher <106377699+harriet-fisher@users.noreply.github.com>
2023-10-24 18:26:26 +0000

    tried

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  erb(:home)
+  erb(:square)
 end
 
 get("/square/new") do
-  erb(:home)
+  erb(:square)
 end
 
 get("/square_root/new") do
@@ -37,4 +37,15 @@ post("/payment/results") do
   payment_denominator = 1 - (1 + (@apr/12))**(-(@years*12))
   @user_payment = (payment_numerator/payment_denominator).round(2)
   erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+post("/random/results") do
+  min = params.fetch("minimum").to_f
+  max = params.fetch("maximum").to_f
+  @user_rand = rand(min..max)
+  erb(:random_res)
 end
