ebf84d846ccddec2aa977e8b8cd1033b70a0999b
harriet-fisher <106377699+harriet-fisher@users.noreply.github.com>
2023-10-24 18:07:28 +0000

    payment

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ get("/square/new") do
 end
 
 get("/square_root/new") do
-  erb(:home)
+  erb(:square_root)
 end
 
 post("/square_root/results") do
@@ -23,4 +23,18 @@ post("/square/results") do
   @number = params.fetch("number").to_i
   @square = @number**2
   erb(:flexible)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+post("/payment/results") do
+  @apr = (params.fetch("user_apr").to_f)/100
+  @years = params.fetch("user_years").to_i
+  @pv = params.fetch("user_pv").to_f
+  payment_numerator = (@apr/12)*@pv
+  payment_denominator = 1 - (1 + (@apr/12))**(-(@years*12))
+  @user_payment = (payment_numerator/payment_denominator).round(2)
+  erb(:payment_results)
 end
